d398922f8487e3b26f38fecbbb2259114d7b1810
mjxaify <itsmdmujeeb@gmail.com>
2026-01-30 12:32:39 +0530

    solved question one

--- src/sprint_1/Question_1.java
+++ src/sprint_1/Question_1.java
@@ -1,7 +1,19 @@
 package sprint_1;
 
+import java.util.Scanner;
+
 public class Question_1 {
     static void main(String[] args) {
-        System.out.println("Sprint 1 Initialised");
+        // Write a program to check whether a number is even or odd.
+
+        System.out.print("Input a number: ");
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+
+        if(num % 2 == 0){
+            System.out.println("Even Number");
+        }else{
+            System.out.println("Odd Number");
+        }
     }
 }
